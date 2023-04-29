36c0c150b6fc88b8bb24fd3a0fd5fb56bc1e97c6
unknown <krishnasuwal2019@gmail.com>
2023-04-29 21:21:57 +0545

    Update plugin description

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,11 +28,15 @@ export default function Home() {
 							<div className="column-2">
 								<h3>PDF Invoice Builder for WooCommerce</h3>
 								<p>
-									Eu minim deserunt et magna. In anim enim reprehenderit dolore
-									consequat id voluptate velit deserunt esse. Voluptate duis
-									sunt nisi sit aliqua in cillum anim reprehenderit labore.
-									Deserunt adipisicing culpa proident qui culpa. Reprehenderit
-									aliquip officia est cupidatat fugiat magna Lorem duis nostrud.
+									With this powerful WooCommerce plugin, you can easily design
+									custom invoices and packing slips with any layout you desire,
+									all through the user-friendly Gutenberg editor. Say goodbye to
+									rigid templates and hello to a more flexible and efficient way
+									of creating professional-looking documents that perfectly
+									match your brand. Our plugin streamlines the entire invoicing
+									and packing slip creation process, saving you valuable time.
+									Try our plugin today and take your invoicing to the next
+									level!
 								</p>
 								<span className="read-more">Read More</span>
 							</div>

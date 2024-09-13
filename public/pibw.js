063b8e71d834dfc504fb1d3d69c3b78198b2c743
Krishna Suwal <krishnasuwal2019@gmail.com>
2024-09-13 18:56:11 +0545

    Fixes

--- public/pibw.js
+++ public/pibw.js
@@ -17,10 +17,23 @@ function run() {
 			jQuery('.pibw-demo-link').attr('href', demo_url);
 		}
 	});
+
+	jQuery('.pibw-docs-link').on('click', function () {
+		if (jQuery('.pibw-docs-link').attr('href') !== doc_url) {
+			window.location.href = doc_url;
+		}
+	});
+
+	jQuery('.pibw-demo-link').on('click', function () {
+		if (jQuery('.pibw-demo-link').attr('href') !== demo_url) {
+			window.location.href = demo_url;
+		}
+	});
 }
 
 if (window.jQuery) {
 	run();
 } else {
+	console.error('jQuery is not ready!');
 	document.getElementById('jquery-script').addEventListener('load');
 }
